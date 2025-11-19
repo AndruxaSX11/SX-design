02dc03181a9190f80867786664d324c65215bbb7
AndruxaSX11 <andrewsav2006@gmail.com>
2025-11-19 22:53:51 +0300

    some cool things

--- index.js
+++ index.js
@@ -47,3 +47,20 @@ cards.forEach((card, index) => {
 })
 
 updateCarousel()
+
+const text = "SXdesign"
+const speed = 200
+const element = document.getElementById("typewriter")
+
+let index = 0
+
+function typeWriter() {
+    if (index < text.length) {
+        element.innerHTML += text[index]
+        index++
+        setTimeout(typeWriter, speed)
+    }
+}
+
+typeWriter()
+
